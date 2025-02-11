7919807a45a84a0b75b4ceaa4358a616e5f01af9
Rivet <139978419+Rivetchan@users.noreply.github.com>
2025-02-11 21:13:31 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,28 @@
-let frequency = 0
-let distance = 0
 let derajat = 0
 let lastKiblatTime = 0
-
+let distance = 0
+let frequency = 0
 basic.forever(function () {
     derajat = input.compassHeading()
-
     // Jika menghadap Kiblat (292°–294°)
     if (derajat > 291 && derajat < 295) {
-        basic.showArrow(ArrowNames.North)
-
+        // Menampilkan "❌" jika tidak menghadap Kiblat
+        basic.showIcon(IconNames.House)
         // Jika sudah lebih dari 2 detik sejak terakhir bunyi, bunyikan lagi
         if (input.runningTime() - lastKiblatTime > 2000) {
             distance = Math.abs(derajat - 293)
             frequency = 1000 + 100 * distance
             pins.analogSetPitchPin(AnalogPin.P0)
             music.ringTone(frequency)
-            basic.pause(2000) // Buzzer berbunyi selama 2 detik
+            // Buzzer berbunyi selama 2 detik
+            basic.pause(2000)
             music.stopAllSounds()
             lastKiblatTime = input.runningTime()
         }
     } else {
+        // Menampilkan "❌" jika tidak menghadap Kiblat
         basic.showIcon(IconNames.No)
-        music.stopAllSounds() // Matikan suara jika tidak menghadap Kiblat
+        // Matikan suara jika tidak menghadap Kiblat
+        music.stopAllSounds()
     }
 })
